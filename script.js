ab4a97e7a46edb0b40c5161f6ac86cb68c41d26a
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-03-07 21:01:15 +0300

    Test
    Test

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonElem = document.getElementById('myBtn');
+ 
+let onButtonClick = function() {
+buttonElem.textContent = 'Спасибо за клик'
+}
+
+buttonElem.addEventListener('click', onButtonClick); 
